b3965cd4cb10340168df74fdb1a43240780d25a8
andyleclair <andy.leclair@gmail.com>
2011-02-02 17:26:03 -0500

    generator creates outfile now

--- lib/baby/generator.rb
+++ lib/baby/generator.rb
@@ -1,7 +1,29 @@
 class Generator
 
+	DEFAULTS = {
+		:instrument => 'violin',
+		:complexity => 20,
+		:key => 'CMaj',
+		:bars => 10,
+		:tempo => 90,
+		:style => 'classical',
+		:range => 'middle'
+	}
+
 	def self.run!(*args)
-		puts "workin, yo"
-		args.each {|a| puts "#{a}\n" }
+		inname, outname = args
+		infile, outfile = [nil, nil]
+
+		if File.exists?(outname)
+			File.delete(outname)
+		end
+		
+		outfile = File.new(outname, 'w+')
+
+		generate!(outfile)
+	end
+
+	def self.generate!(outfile)
+		
 	end
 end
