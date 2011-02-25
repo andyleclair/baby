5cde9dda8253e40d2fbcb7d4f1d492fc45b85854
Dennis Zografos <dzog@ccs.neu.edu>
2011-02-25 11:44:01 -0500

    added beginnings of option parsing (for debug mostly)

--- lib/baby.rb
+++ lib/baby.rb
@@ -5,15 +5,32 @@ Dir[File.join('lib', 'baby', 'instruments', '*.rb')].each {|instr| load instr}
 require 'baby/generator'
 require 'baby/lilypond'
 require 'citrus'
+require 'optparse'
 
 class Baby
     class Got
         class Bach
     
+            options = {}
+            optparse = OptionParser.new do|opts|
+                options[:verbose] = false
+                opts.on('-v', '--verbose', 'Print debug information' ) do
+                    option[:verbose] = true
+                end
+                opts.on('--set-instrument', 'Override instrument setting' ) do
+                    puts "blah"
+                end
+                opts.on('--set-key', 'Override key setting' ) do
+                    puts "blah"
+                end
+            end
+
+            optparse.parse!
+
             SYNTAX = <<-SYNTAX
 
 Baby Got Bach:
-    usage: ./c4500 <input doc> <output filename>
+    usage: #{ $0 } <input doc> <output filename>
 
             SYNTAX
 
