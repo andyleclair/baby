25dce19205310559604ea4411abde507eb8de9c8
andyleclair <andy.leclair@gmail.com>
2011-03-21 18:35:19 -0400

    adding devtest script, difficulty now figures into note length, VERY SIMPLE function to do so

--- lib/baby.rb
+++ lib/baby.rb
@@ -5,51 +5,50 @@ Dir[File.join('lib', 'baby', 'instruments', '*.rb')].each {|instr| load instr}
 require 'baby/lilypond'
 require 'baby/options'
 require 'baby/generator'
-require 'baby/theory'
 require 'optparse'
 
 class Baby
-    class Got
-        class Bach
-    
-            COPTS = {}
-            optparse = OptionParser.new do|opts|
-			    opts.banner = "usage: #{ $0 } [options] <input> <output>"
-                COPTS[:verbose] = false
-                opts.on('-v', '--verbose', 'Print debug information' ) do
-                    COPTS[:verbose] = true
-                end
-                opts.on('--set-instrument INSTRUMENT', 'Force `instrument\' parameter' ) do|instrument|
-                    COPTS[:set_instrument] = instrument
-                end
-                opts.on('--set-timesig TIMESIG', 'Force `time signature\' parameter' ) do|timesig|
-                    COPTS[:set_timesig] = timesig
-                end
-                opts.on('--set-key KEY', 'Force `key\' parameter' ) do|key|
-                    COPTS[:set_key] = key
-                end
-                opts.on('--set-length LENGTH', 'Force `length\' parameter' ) do|length|
-                    COPTS[:set_length] = length
-                end
-				opts.on('-h','--help','Display this screen') do
-				    puts opts
-					exit
-				end
-            end
+  class Got
+    class Bach
 
-			RUNSYNTAX = optparse.help()
-            optparse.parse!
+      COPTS = {}
+      optparse = OptionParser.new do|opts|
+	opts.banner = "usage: #{ $0 } [options] <input> <output>"
+	COPTS[:verbose] = false
+	opts.on('-v', '--verbose', 'Print debug information' ) do
+	  COPTS[:verbose] = true
+	end
+	opts.on('--set-instrument INSTRUMENT', 'Force `instrument\' parameter' ) do|instrument|
+	  COPTS[:set_instrument] = instrument
+	end
+	opts.on('--set-timesig TIMESIG', 'Force `time signature\' parameter' ) do|timesig|
+	  COPTS[:set_timesig] = timesig
+	end
+	opts.on('--set-key KEY', 'Force `key\' parameter' ) do|key|
+	  COPTS[:set_key] = key
+	end
+	opts.on('--set-length LENGTH', 'Force `length\' parameter' ) do|length|
+	  COPTS[:set_length] = length
+	end
+	opts.on('-h','--help','Display this screen') do
+	  puts opts
+	  exit
+	end
+      end
 
-            def self.run!(*args)
-                case args.length
-                    when 2
-                        Generator.run!(COPTS, *args)
-                    else
-                        puts RUNSYNTAX
-						puts COPTS
-                end
-                return 0
-            end
-        end
+      RUNSYNTAX = optparse.help()
+      optparse.parse!
+
+      def self.run!(*args)
+	case args.length
+	when 2
+	  Generator.run!(COPTS, *args)
+	else
+	  puts RUNSYNTAX
+	  puts COPTS
+	end
+	return 0
+      end
     end
+  end
 end
