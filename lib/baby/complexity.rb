f7e7baf8377e6ee2f3fd8e4e00809c06224aa831
Dennis Zografos <dzog@ccs.neu.edu>
2011-03-22 17:40:46 -0400

    updated complexity values

--- lib/baby/complexity.rb
+++ lib/baby/complexity.rb
@@ -5,13 +5,13 @@ class Complexity
     c = Complexity.new
     if n < 21
       c.pitch_delta = 2
-      c.len_probs = [0.2,0.2,0.6,0,0]
+      c.len_probs = [0.5,0.4,0.1,0,0]
     elsif n < 51
       c.pitch_delta = 4
-      c.len_probs = [0.1,0.1,0.6,0.2,0]
+      c.len_probs = [0.3,0.45,0.2,0.05,0]
     elsif n < 81
       c.pitch_delta = 8
-      c.len_probs = [0.05,0.05,0.5,0.25,0.05]
+      c.len_probs = [0.05,0.1,0.5,0.2,0.05]
     else
       c.pitch_delta = 10
       c.len_probs = [0.05,0.05,0.35,0.35,0.1]
