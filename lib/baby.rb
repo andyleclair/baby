30b24243fa53a19672ba337712fc8a5093d97dd0
andyleclair <andy.leclair@gmail.com>
2011-04-13 22:18:15 -0400

    cleaning up copts

--- lib/baby.rb
+++ lib/baby.rb
@@ -6,48 +6,17 @@ require 'baby/lilypond'
 require 'baby/options'
 require 'baby/generator'
 require 'baby/complexity'
-require 'optparse'
 require 'erb'
 
 class Baby
   class Got
     class Bach
-
-      COPTS = {}
-      optparse = OptionParser.new do|opts|
-	opts.banner = "usage: #{ $0 } [options] <input> <output>"
-	COPTS[:verbose] = false
-	opts.on('-v', '--verbose', 'Print debug information' ) do
-	  COPTS[:verbose] = true
-	end
-	opts.on('--set-instrument INSTRUMENT', 'Force `instrument\' parameter' ) do|instrument|
-	  COPTS[:set_instrument] = instrument
-	end
-	opts.on('--set-timesig TIMESIG', 'Force `time signature\' parameter' ) do|timesig|
-	  COPTS[:set_timesig] = timesig
-	end
-	opts.on('--set-key KEY', 'Force `key\' parameter' ) do|key|
-	  COPTS[:set_key] = key
-	end
-	opts.on('--set-length LENGTH', 'Force `length\' parameter' ) do|length|
-	  COPTS[:set_length] = length
-	end
-	opts.on('-h','--help','Display this screen') do
-	  puts opts
-	  exit
-	end
-      end
-
-      RUNSYNTAX = optparse.help()
-      optparse.parse!
-
       def self.run!(*args)
 	case args.length
 	when 2
-	  Generator.run!(COPTS, *args)
+	  Generator.run!(*args)
 	else
-	  puts RUNSYNTAX
-	  puts COPTS
+	  puts "usage: ./c4500 <input file> <output file name>"
 	end
 	return 0
       end
--- lib/baby/generator.rb
+++ lib/baby/generator.rb
@@ -1,26 +1,12 @@
 class Generator
 
-  def self.run!(copts = {}, *args)
+  def self.run!(*args)
     inname, outname = args
-    options = Options.parse(inname)
+    options = Options.parse(inname).to_hash
 
-    #override = lambda {|x, y|
-    #	options.call(x, copts[y]) if copts.key?(y)
-    #}
-
-    #override.call(:instrument, :set_instrument)
-    #override.call(:time, :set_timesig)
-    #override.call(:length, :set_length)
-    #override.call(:key, Options.parse_key(copts[:set_key])) if copts.key?(:set_key)
-    #override.
-
-    generate!(outname, options.to_hash)
-  end
-
-  def self.generate!(outfile, options)
     classname = options[:instrument].split(" ").each{ |s| s.capitalize! }.join
     instrument = Kernel.const_get(classname)
-    File.open(outfile, 'w') do |f|
+    File.open(outname, 'w') do |f|
       f.write LilyPond::create_doc(options, instrument)
     end
   end
